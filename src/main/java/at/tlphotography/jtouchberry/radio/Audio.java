707e8b9618493a3db1aeb907738436cf961a758a
Herschdorfer <herschdorfer@gmail.com>
2017-03-24 07:51:03 +0100

    Update Audio.java

--- src/main/java/at/tlphotography/jtouchberry/radio/Audio.java
+++ src/main/java/at/tlphotography/jtouchberry/radio/Audio.java
@@ -33,8 +33,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import at.tlphotography.jtouchberry.ScheduledTasks;
-
 @Component
 public class Audio {
 
