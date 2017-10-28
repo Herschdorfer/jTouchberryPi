019a11ddc52842b6e672ad539310a9810cd9c95b
Herschdorfer <herschdorfer@gmail.com>
2017-10-28 16:38:00 +0200

    cleaunp

---
+++ main-app/src/main/java/at/tlphotography/jtouchberry/radio/db/Radio.java
@@ -0,0 +1,61 @@
+package at.tlphotography.jtouchberry.radio.db;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "radio")
+public class Radio {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@NotNull
+	private String name;
+
+	@NotNull
+	private String uri;
+
+	public Radio() {
+	    //need to be public
+	}
+
+	public Radio(long id) {
+		this.id = id;
+	}
+
+	public Radio(String uri, String name) {
+		this.uri = uri;
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long value) {
+		this.id = value;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	public void setSet(String uri) {
+		this.uri = uri;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String value) {
+		this.name = value;
+	}
+
+}
---
+++ main-app/src/main/java/at/tlphotography/jtouchberry/radio/ws/RadioController.java
@@ -0,0 +1,100 @@
+package at.tlphotography.jtouchberry.radio.ws;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.tlphotography.jtouchberry.radio.Audio;
+import at.tlphotography.jtouchberry.radio.RadioPlayer;
+import at.tlphotography.jtouchberry.radio.db.Radio;
+import at.tlphotography.jtouchberry.radio.db.RadioDao;
+
+@RestController
+public class RadioController {
+
+    Logger LOGGER = LogManager.getLogger(RadioController.class);
+
+    @Autowired
+    private RadioDao radioDao;
+
+    @Autowired
+    private RadioPlayer player;
+
+    @Autowired
+    private Audio audio;
+
+    private Float volume;
+
+    @RequestMapping("/radio/")
+    @ResponseBody
+    public Iterable<Radio> list() {
+	return radioDao.findAll();
+    }
+
+    @RequestMapping("/radio/play")
+    @ResponseBody
+    public String play(Long id) {
+	player.play(radioDao.findOne(id).getUri());
+	return "OK";
+    }
+
+    @RequestMapping("/radio/stop")
+    @ResponseBody
+    public String stop() {
+	player.stop();
+	return "OK";
+    }
+
+    @RequestMapping("/radio/delete")
+    @ResponseBody
+    public String delete(Long id) {
+	radioDao.delete(id);
+	return "OK";
+    }
+
+    @RequestMapping("/radio/add")
+    @ResponseBody
+    public ResponseEntity<String> add(String uri, String name) {
+
+	LOGGER.info("got invoked with " + uri + name);
+
+	if (uri.isEmpty() || name.isEmpty()) {
+	    return ResponseEntity.badRequest().body("name and Uri have to be set");
+	}
+
+	Radio radio = null;
+	try {
+	    radio = new Radio(uri, name);
+	    radioDao.save(radio);
+	} catch (Exception ex) {
+	    LOGGER.error(ex);
+	    return ResponseEntity.badRequest().body(ex.getLocalizedMessage());
+	}
+	return ResponseEntity.ok("Radio succesfully created! (id = " + radio.getId() + ")");
+    }
+
+    @RequestMapping("/radio/setVolume")
+    @ResponseBody
+    public String setVolume(Float volume) {
+	this.volume = volume;
+	audio.setMasterOutputVolume(volume);
+	return "OK";
+    }
+
+    @RequestMapping("/radio/getVolume")
+    @ResponseBody
+    public Float getVolume() {
+	return volume;
+    }
+
+    @RequestMapping("/radio/getMixer")
+    @ResponseBody
+    public String setMixer() {
+
+	return Audio.getHierarchyInfo();
+    }
+}
